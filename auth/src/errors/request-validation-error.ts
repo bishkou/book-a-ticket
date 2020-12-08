866eea5af6dcdd93932d81e4acd0d73ae15f1ff7
bishkou <chedyhammami@yahoo.com>
2020-12-08 19:30:28 +0100

    valid error

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -11,7 +11,6 @@ export class RequestValidationError extends CustomError {
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
 
-
   serializeErrors() {
     return this.errors.map(err => {
       return { message: err.msg, field: err.param };
