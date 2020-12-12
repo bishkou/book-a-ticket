93df0f6aa2dca736df37cc5efcc535f8a5beb431
bishkou <chedyhammami@yahoo.com>
2020-12-12 17:34:52 +0100

    error handler

--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -11,7 +11,6 @@ export const errorHandler = (
     return res.status(err.statusCode).send({ errors: err.serializeErrors() });
   }
 
-
   res.status(400).send({
     errors: [{ message: 'Something went wrong' }]
   });
