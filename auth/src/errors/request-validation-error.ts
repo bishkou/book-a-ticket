45e46ee67b5b9f3718a17e1195d6899413ba0c62
bishkou <chedyhammami@yahoo.com>
2020-12-12 17:32:44 +0100

    validation

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -7,7 +7,6 @@ export class RequestValidationError extends CustomError {
   constructor(public errors: ValidationError[]) {
     super('Invalid request parameters');
 
-
     // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
