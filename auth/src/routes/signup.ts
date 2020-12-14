c594fca574805c5d8256b1a16b10c15e42b222e7
bishkou <chedyhammami@yahoo.com>
2020-12-14 10:49:31 +0100

    request validator

--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,5 +1,6 @@
 import express, { Request, Response } from 'express';
 import { body, validationResult } from 'express-validator';
+import jwt from 'jsonwebtoken';
 import { User } from '../models/user';
 import { RequestValidationError } from '../errors/request-validation-error';
 import { BadRequestError } from '../errors/bad-request-error';
@@ -34,6 +35,17 @@ router.post(
 
     const user = User.build({ email, password });
     await user.save();
+
+    // Generate JWT
+    const userJWT = jwt.sign({
+      id: user.id,
+      email: user.email
+    }, process.env.JWT_KEY! );
+
+    // Store it on session object
+    req.session = {
+      jwt: userJWT
+    };
 
     res.status(201).send(user);
   }
