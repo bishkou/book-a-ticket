dacf820f250a3aca14810ff5f4d2a05ce1259e05
bishkou <chedyhammami@yahoo.com>
2020-12-17 20:58:27 +0100

    tickets service

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -2,7 +2,6 @@ import express from 'express';
 import 'express-async-errors';
 import { json } from 'body-parser';
 import cookieSession from 'cookie-session';
-
 import { currentUserRouter } from './routes/current-user';
 import { errorHandler, NotFoundError } from '@chticket/common';
 import {signinRouter} from "./routes/signin";
