b06be92c763e6d7842af07707a831a0eff9a25e8
bishkou <chedyhammami@yahoo.com>
2021-01-02 20:17:13 +0100

    listeners

--- tickets/src/routes/__test__/new.test.ts
+++ tickets/src/routes/__test__/new.test.ts
@@ -20,7 +20,6 @@ it('returns a status other than 401 if the user is signed in', async () => {
         .send({});
 
     expect(response.status).not.toEqual(401);
-
 });
 
 it('returns an error if an invalid title is provided', async () => {
