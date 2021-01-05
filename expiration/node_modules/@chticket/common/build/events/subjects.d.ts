f0b7f869b977f9c2ef9531fd8cb8a3032f624073
bishkou <chedyhammami@yahoo.com>
2021-01-05 12:56:49 +0100

    expiration setup

---
+++ expiration/node_modules/@chticket/common/build/events/subjects.d.ts
@@ -0,0 +1,7 @@
+export declare enum Subjects {
+    TicketCreated = "ticket:created",
+    TicketUpdated = "ticket:updated",
+    OrderCreated = "order:created",
+    OrderCancelled = "order:cancelled",
+    ExpirationComplete = "expiration:complete"
+}
