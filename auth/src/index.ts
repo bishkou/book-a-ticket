d90053953ee5bbbc37021f8e7bde0fb50c73776b
bishkou <chedyhammami@yahoo.com>
2021-01-11 16:11:09 +0100

    added message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -4,6 +4,7 @@ import { app } from './app';
 
 const start = async () => {
   console.log('starting')
+  console.log('starting')
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
